8c19d9c05bf9872508b525cf2a8b3300b250d637
Daniel Winship <danielwinship@icloud.com>
2018-01-21 17:57:30 -0500

    creates pop view controller

---
+++ Pixel-City/Controller/PopVC.swift
@@ -0,0 +1,27 @@
+//
+//  PopVC.swift
+//  Pixel-City
+//
+//  Created by Daniel Winship on 1/21/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    
+
+    
+
+}
